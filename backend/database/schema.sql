2ab2cf9ee46ad026f4d927feae2e2bc1a3b7a3c5
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2023-12-07 20:50:26 +1100

    🔥 update schema, er->sql mapping

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,1 +1,75 @@
+create table Account (
+	id		serial,
+	givenName	text			not null,
+	familyName	text			not null,
+	primary key	(id)
+);
 
+create table Adult (
+	accountID	serial,
+	email		text			not null,
+	primary key	(accountID),
+	foreign key	(accountID)		references	Account(id)
+);
+
+create table Student (
+	studentID	serial,
+	primary key	(studentID),
+	foreign key	(studentID)		references	Account(id)
+);
+
+create table Administrator (
+	adminID		serial,
+	primary key	(adminID),
+	foreign key	(adminID)		references	Account(id)
+);
+
+create table Teaches (
+	adminID		serial,
+	studentID	serial,
+	foreign key	(adminID)		references	Administrator(id),
+	foreign key	(studentID)		references	Student(id),
+	primary key	(adminID, studentID)
+);
+
+create table Guardian (
+	guardianID	serial,
+	primary key	(guardianID),
+	foreign key	(guardianID)		references	Account(id)
+);
+
+create table HasChild (
+	studentID	serial,
+	guardianID	serial,
+	foreign key	(studentID)		references	Student(id),
+	foreign key	(guardianID)		references	Guardian(id)
+	primary key	(studentID, guardianID)
+);
+
+create table OwnsWebsite (
+	-- todo: only an administrator or student can own websites.
+	-- 'accountID' should only reference either Student(id) or Administrator(id)
+	-- the er model should also be updated to reflect that a student only owns
+	-- one website, but an administrator can manage every website owned by some students.
+	accountID	serial,
+	websiteID	serial,
+	foreign key	(accountID)		references	Account(id),
+	foreign key	(websiteID)		references	Website(id)
+	primary key	(accountID, websiteID)
+);
+
+create table Website (
+	websiteID	serial,
+	title		text,
+	primary key	(websiteID)
+);
+
+create table Webpage (
+	-- todo: depict relationship between website and webpage
+	webpageID	serial,
+	title		text,
+	filename	text,
+	contents	text,	-- url to HTML file
+				-- possibly add stylesheet and script? idk
+	primary key	(webpageID)
+);
