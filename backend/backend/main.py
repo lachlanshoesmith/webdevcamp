0b97294562b15463c3d214444ec900c30c79a983
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2023-12-13 20:59:20 +1100

    🔒 add basic auth routes

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,15 +1,61 @@
 import os
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from dotenv import load_dotenv
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from typing import Annotated
+from pydantic import BaseModel
 
 load_dotenv()
 
+# much of the authentication code is borrowed from fastapi's documentation
+# https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/
+
 SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+class Token(BaseModel):
+    accessToken: str
+    tokenType: str
+
 
 app = FastAPI()
 
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl='token')
 
-# user management
+
+# def createAccessToken(data: dict, expiresDelta: timedelta | None = None):
+#     '''
+#     Creates an access token by encoding the given data using JWT.
+
+#     Args:
+#         data (dict): The data to be encoded into the access token.
+#         expiresDelta (timedelta | None, optional): The time delta representing the expiration time of the access token. Defaults to None.
+
+#     Returns:
+#         str: The encoded access token.
+
+#     Raises:
+#         <ExceptionType>: <Description of the exception raised, if any.>
+#     '''
+#     toEncode = data.copy()
+#     if expiresDelta:
+#         expire = datetime.utcnow() + expiresDelta
+#     else:
+#         expire = datetime.utcnow() + timedelta(minutes=15)
+#     toEncode.update({'exp': expire})
+#     encodedJWT = jwt.encode(toEncode, SECRET_KEY, algorithm=ALGORITHM)
+#     return encodedJWT
+
+# user management routes
+
+
+@app.get('/items/')
+async def readItems(token: Annotated[str, Depends(oauth2Scheme)]):
+    return {'token': token}
 
 
 @app.post('/register')
