f58f972d7224709ab45ac87067fe460cfeb24726
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2023-12-29 23:15:34 +1100

    🧪 add simple student registration tests

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -3,3 +3,23 @@ from fastapi.testclient import TestClient
 from backend import main
 
 client = TestClient(main.app)
+
+registering_student: main.RegisteringUser = {
+    'username': 'neffieta',
+    'given_name': 'Neffie Etta',
+    'family_name': 'Denile',
+    'password': 'password123',
+    'account_type': 'student'
+}
+
+
+def test_register_student_endpoint():
+    response = client.post('/register/student', json=registering_student)
+    assert response.status_code == 200
+
+
+def test_register_student_at_incorrect_endpoint():
+    response = client.post('/register', json=registering_student)
+    assert response.status_code == 400
+    assert response.json()[
+        'detail'] == 'Students cannot register via /register. Use /register/student.'
