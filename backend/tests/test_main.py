35f33eef5da5b60f70f07011ca4b2a685a8e49b8
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-01-04 08:03:16 +1100

    🧪 add student edge case tests

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from httpx import AsyncClient
 from asgi_lifespan import LifespanManager
+from copy import deepcopy
 
 from backend import main
 from .testdata import TestData as d
@@ -22,12 +23,12 @@ async def test_register_student(test_db):
     res = await register_administrator()
     assert res.status_code == 200
     
-    student = d.registering_student
+    student = deepcopy(d.registering_student)
     student['administrator_id'] = res.json()['account_id']
 
     async with LifespanManager(main.app):
         async with AsyncClient(app=main.app, base_url='http://test') as ac:
-            response = await ac.post('/register/student', json=d.registering_student)
+            response = await ac.post('/register/student', json=student)
         assert response.status_code == 200, response.text
         assert 'student_id' in response.json() 
 
@@ -56,6 +57,78 @@ async def test_register_student_at_incorrect_endpoint_with_incorrect_data_model(
         async with AsyncClient(app=main.app, base_url='http://test') as ac:
             response = await ac.post('/register', json=d.registering_student)
         assert response.status_code == 422, response.text
+
+@pytest.mark.anyio
+async def test_register_student_without_administrator_id(test_db):
+    student = deepcopy(d.registering_student)
+    del student['administrator_id']
+
+    async with LifespanManager(main.app):
+        async with AsyncClient(app=main.app, base_url='http://test') as ac:
+            response = await ac.post('/register/student', json=student)
+        assert response.status_code == 422, response.text
+
+@pytest.mark.anyio
+async def test_register_student_without_username(test_db):
+    res = await register_administrator()
+    assert res.status_code == 200
+    
+    student = deepcopy(d.registering_student)
+    del student['user']['username']
+
+    async with LifespanManager(main.app):
+        async with AsyncClient(app=main.app, base_url='http://test') as ac:
+            response = await ac.post('/register/student', json=student)
+        assert response.status_code == 422, response.text
+
+@pytest.mark.anyio
+async def test_register_student_without_password(test_db):
+    res = await register_administrator()
+    assert res.status_code == 200
+    
+    student = deepcopy(d.registering_student)
+    del student['user']['hashed_password']
+
+    async with LifespanManager(main.app):
+        async with AsyncClient(app=main.app, base_url='http://test') as ac:
+            response = await ac.post('/register/student', json=student)
+        assert response.status_code == 422, response.text
+
+@pytest.mark.anyio
+async def test_register_student_without_names(test_db):
+    res = await register_administrator()
+    assert res.status_code == 200
+    
+    student = deepcopy(d.registering_student)
+    del student['user']['given_name']
+    del student['user']['family_name']
+
+    async with LifespanManager(main.app):
+        async with AsyncClient(app=main.app, base_url='http://test') as ac:
+            response = await ac.post('/register/student', json=student)
+        assert response.status_code == 422, response.text
+
+@pytest.mark.anyio
+async def test_register_student_without_data(test_db):
+    async with LifespanManager(main.app):
+        async with AsyncClient(app=main.app, base_url='http://test') as ac:
+            response = await ac.post('/register/student', json={})
+        assert response.status_code == 422, response.text
+
+@pytest.mark.anyio
+async def test_register_student_with_long_username(test_db):
+    res = await register_administrator()
+    assert res.status_code == 200
+    
+    student = deepcopy(d.registering_student)
+    student['administrator_id'] = res.json()['account_id']
+    student['user']['username'] = 'abcabcabcabcabcabcabcabc'
+
+    async with LifespanManager(main.app):
+        async with AsyncClient(app=main.app, base_url='http://test') as ac:
+            response = await ac.post('/register/student', json=student)
+        assert response.status_code == 400, response.text
+        assert response.json()['detail'] == 'Username is too long.'
 
 
 @pytest.mark.anyio
