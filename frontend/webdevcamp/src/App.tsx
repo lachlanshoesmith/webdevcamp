8cb0a9fc17c63bb7ed4ae9cae0eb8681d31c605b
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-01-08 18:28:46 +1100

    🌟 add frontend 🤯

---
+++ frontend/webdevcamp/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>webdevcamp</h1>
+      <p>Coming very soon. 🌞</p>
+      <a href="https://github.com/lachlanshoesmith/webdevcamp">GitHub</a>
+      <p>By <a href="https://github.com/lachlanshoesmith">Lachlan Shoesmith</a></p>
+    </>
+  )
+}
+
+export default App
