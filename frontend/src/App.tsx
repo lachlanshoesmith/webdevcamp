80ff71a4695428f40f7b1970a00fd3bd5f67eaab
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-01-10 17:48:39 +1100

    🕸 add website uploading related tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1>webdevcamp</h1>
-      <p>Coming very soon. 🌞</p>
-      <a href="https://github.com/lachlanshoesmith/webdevcamp">GitHub</a>
-      <p>By <a href="https://github.com/lachlanshoesmith">Lachlan Shoesmith</a></p>
+      <header>
+        <h1>webdevcamp</h1>
+      </header>
+      <main>
+        <p>Welcome! Please log in.</p>
+        <button>Log in</button>
+        <button>Register</button>
+      </main>
+      <footer>
+        <a href='https://github.com/lachlanshoesmith/webdevcamp'>GitHub</a>
+        <p>
+          By <a href='https://github.com/lachlanshoesmith'>Lachlan Shoesmith</a>
+        </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
