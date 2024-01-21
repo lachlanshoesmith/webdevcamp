5ee65042c92c03c1a0f1a9ededbbf0d2e9f2285f
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-01-21 16:36:50 +1100

    🔑 add login api call

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,15 +1,52 @@
-import { Form } from 'react-router-dom';
 import Button from '../components/Button/Button';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-function login() {
+type Inputs = {
+  username: string;
+  password: string;
+};
+
+export default function Login() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    fetch('https://webdevcamp.fly.dev/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log(res.status);
+      if (res.ok) console.log(res.json());
+    });
+  };
   return (
     <>
       <h1>Login Page</h1>
-      <Form method="get">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          {...register('username', {
+            required: true,
+            minLength: 3,
+            maxLength: 20,
+          })}
+          aria-invalid={errors.username ? 'true' : 'false'}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          {...(register('password'),
+          {
+            required: true,
+            minLength: 8,
+          })}
+          aria-invalid={errors.password ? 'true' : 'false'}
+          placeholder="Password"
+        />
         <Button type="submit">Login</Button>
-      </Form>
+      </form>
     </>
   );
 }
-
-export default login;
